939473f7edbdda00c3a252c335046b36e9275145
Ernst-Georg Schmid <333776+ergo70@users.noreply.github.com>
2023-01-13 20:26:24 +0100

    Update le_meldungen_API.py
    + slowapi

--- le_meldungen_API.py
+++ le_meldungen_API.py
@@ -1,51 +1,64 @@
 import sqlite3
-from fastapi import FastAPI, HTTPException
+import uvicorn
+from typing import List, Union
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-
-app = FastAPI()
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
 
 con = sqlite3.connect('file:./LEMeldungen.db?mode=ro', uri=True)
 select_part = """pzn, enr, meldungsart, beginn, ende, datum_der_letzten_meldung, art_des_grundes, arzneimittelbezeichnung, atc_code, wirkstoffe, krankenhausrelevant, zulassungsinhaber, telefon, email, grund, anmerkung_zum_grund, alternativpraeparat, datum_der_erstmeldung, info_an_fachkreise, created"""
 
+limiter = Limiter(key_func=get_remote_address)
+app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 
 class LEMeldung(BaseModel):
-    PZN: str | None = None
-    ENR: str | None = None
-    Meldungsart: str | None = None
+    PZN: Union[str, None] = None
+    ENR: Union[str, None] = None
+    Meldungsart: Union[str, None] = None
     Beginn: str
     Ende: str
-    Datum_der_letzen_Meldung: str | None = None
+    Datum_der_letzen_Meldung: Union[str, None] = None
     Art_des_Grundes: str
     Arzneimittelbezeichnung: str
     ATC_Code: str
     Wirkstoffe: str
     Krankenhausrelevant: bool
     Zulassungsinhaber: str
-    Telefon: str | None = None
-    EMail: str | None = None
+    Telefon: Union[str, None] = None
+    EMail: Union[str, None] = None
     Grund: str
-    Anmerkung_zum_Grund: str | None = None
-    Alternativpraeparat: str | None = None
+    Anmerkung_zum_Grund: Union[str, None] = None
+    Alternativpraeparat: Union[str, None] = None
     Datum_der_Erstmeldung: str
     Info_an_Fachkreise: str
     Erzeugt_am: str
 
+class LEMeldungen(BaseModel):
+    le_meldungen: List[LEMeldung] = []    
 
-@app.get("/all/")
-async def find() -> list[LEMeldung]:
+
+@app.get("/all/", response_model=LEMeldungen)
+@limiter.limit("5/minute")
+async def all(request: Request) -> LEMeldungen:
     cur = con.cursor()
 
     cur.execute(
         """SELECT {} FROM le_meldungen;""".format(select_part))
 
-    result = [LEMeldung(PZN=r[0], ENR=r[1], Meldungsart=r[2], Beginn=r[3], Ende=r[4], Datum_der_letzten_Meldung=r[5], Art_des_Grundes=r[6], Arzneimittelbezeichnung=r[7], ATC_Code=r[8], Wirkstoffe=r[9],
-                        Krankenhausrelevant=(r[10] == 1), Zulassungsinhaber=r[11], Telefon=r[12], EMail=r[13], Grund=r[14], Anmerkung_zum_Grund=r[15], Alternativpraeparat=r[16], Datum_der_Erstmeldung=r[17], Info_an_Fachkreise=r[18], Erzeugt_am=r[19]) for r in cur]
+    result = LEMeldungen(le_meldungen=[LEMeldung(PZN=r[0], ENR=r[1], Meldungsart=r[2], Beginn=r[3], Ende=r[4], Datum_der_letzten_Meldung=r[5], Art_des_Grundes=r[6], Arzneimittelbezeichnung=r[7], ATC_Code=r[8], Wirkstoffe=r[9],
+                        Krankenhausrelevant=(r[10] == 1), Zulassungsinhaber=r[11], Telefon=r[12], EMail=r[13], Grund=r[14], Anmerkung_zum_Grund=r[15], Alternativpraeparat=r[16], Datum_der_Erstmeldung=r[17], Info_an_Fachkreise=r[18], Erzeugt_am=r[19]) for r in cur])
 
     return result
 
 
-@app.get("/find/")
-async def find(erzeugt_von: str | None = None, erzeugt_bis: str | None = None, pzn: str | None = None, enr: str | None = None, meldungsart: str | None = None, beginn_von: str | None = None, beginn_bis: str | None = None, ende_von: str | None = None, ende_bis: str | None = None, letzte_meldung_von: str | None = None, letzte_meldung_bis: str | None = None, arzneimittel: str | None = None, atc_code: str | None = None, wirkstoffe: str | None = None, krankenhausrelevant: bool | None = None) -> list[LEMeldung]:
+@app.get("/find/", response_model=LEMeldungen)
+@limiter.limit("5/minute")
+async def find(request: Request, erzeugt_von: Union[str, None] = None, erzeugt_bis: Union[str, None] = None, pzn: Union[str, None] = None, enr: Union[str, None] = None, meldungsart: Union[str, None] = None, beginn_von: Union[str, None] = None, beginn_bis: Union[str, None] = None, ende_von: Union[str, None] = None, ende_bis: Union[str, None] = None, letzte_meldung_von: Union[str, None] = None, letzte_meldung_bis: Union[str, None] = None, arzneimittel: Union[str, None] = None, atc_code: Union[str, None] = None, wirkstoffe: Union[str, None] = None, krankenhausrelevant: Union[bool, None] = None) -> LEMeldungen:
     and_part = []
     params = []
 
@@ -121,7 +134,10 @@ async def find(erzeugt_von: str | None = None, erzeugt_bis: str | None = None, p
     cur.execute(sql,
                 params)
 
-    result = [LEMeldung(PZN=r[0], ENR=r[1], Meldungsart=r[2], Beginn=r[3], Ende=r[4], Datum_der_letzten_Meldung=r[5], Art_des_Grundes=r[6], Arzneimittelbezeichnung=r[7], ATC_Code=r[8], Wirkstoffe=r[9],
-                        Krankenhausrelevant=(r[10] == 1), Zulassungsinhaber=r[11], Telefon=r[12], EMail=r[13], Grund=r[14], Anmerkung_zum_Grund=r[15], Alternativpraeparat=r[16], Datum_der_Erstmeldung=r[17], Info_an_Fachkreise=r[18], Erzeugt_am=r[19]) for r in cur]
+    result = LEMeldungen(le_meldungen=[LEMeldung(PZN=r[0], ENR=r[1], Meldungsart=r[2], Beginn=r[3], Ende=r[4], Datum_der_letzten_Meldung=r[5], Art_des_Grundes=r[6], Arzneimittelbezeichnung=r[7], ATC_Code=r[8], Wirkstoffe=r[9],
+                        Krankenhausrelevant=(r[10] == 1), Zulassungsinhaber=r[11], Telefon=r[12], EMail=r[13], Grund=r[14], Anmerkung_zum_Grund=r[15], Alternativpraeparat=r[16], Datum_der_Erstmeldung=r[17], Info_an_Fachkreise=r[18], Erzeugt_am=r[19]) for r in cur])
 
     return result
+
+if __name__ == "__main__":
+    uvicorn.run("le_meldungen_API:app", host='0.0.0.0', port=80, reload=False)

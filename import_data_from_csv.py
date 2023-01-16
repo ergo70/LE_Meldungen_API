030c48c6366f09af61b1d5037c00e6b711fa2419
Ernst-Georg Schmid <333776+ergo70@users.noreply.github.com>
2023-01-16 13:54:23 +0100

    Update import_data_from_csv.py
    % Automatic download with Selenium, Firefox and Geckodriver.

--- import_data_from_csv.py
+++ import_data_from_csv.py
@@ -2,15 +2,28 @@
 
 import csv
 import sqlite3
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from glob import glob
+from os import remove
 
-SOURCE = './source_to_import.csv'
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options, executable_path=r'./geckodriver')
 
-con = sqlite3.connect('file:./LEMeldungen.db?mode=rw', uri=True)
-cur = con.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS le_import (col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, co13,col14,col15,col16,col17,col18,col19, col20);""")
-cur.execute("""CREATE TABLE IF NOT EXISTS le_meldungen (source TEXT NOT NULL,pzn TEXT NULL,enr TEXT NULL,meldungsart TEXT NULL,beginn TEXT NOT NULL,ende TEXT NOT NULL,datum_der_letzten_meldung TEXT NULL,art_des_grundes TEXT NOT NULL,arzneimittelbezeichnung TEXT NOT NULL,atc_code TEXT NOT NULL,wirkstoffe TEXT NOT NULL,krankenhausrelevant INTEGER NOT NULL,zulassungsinhaber TEXT NOT NULL,telefon TEXT NULL,email TEXT NULL,grund TEXT NOT NULL,anmerkung_zum_grund TEXT NULL,alternativpraeparat TEXT NULL,datum_der_erstmeldung TEXT NOT NULL,info_an_fachkreise TEXT NOT NULL,created TEXT NOT NULL) STRICT;""")
+driver.get(
+    "https://anwendungen.pharmnet-bund.de/lieferengpassmeldungen/faces/public/meldungen.xhtml")
+assert "Lieferengpassmeldungen" in driver.title
+elem = driver.find_element(By.NAME, "meldungenForm:j_idt175")
+elem.click()
+sleep(30)
+driver.close()
 
-with open('{}'.format(SOURCE), 'r') as fin:
+SOURCE = glob("../Downloads/*.csv")[0]
+
+with open('{}'.format(SOURCE), 'r', encoding="ISO-8859-1") as fin:
     to_db = []
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin, delimiter=';')
@@ -27,9 +40,21 @@ with open('{}'.format(SOURCE), 'r') as fin:
 
         to_db.append(tuple(row))
 
-cur.executemany("""INSERT INTO le_import (col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, co13,col14,col15,col16,col17,col18,col19, col20) VALUES (?,?, ?, ?,?,?,?,?,?,?,?,CASE WHEN ? = 'ja' THEN TRUE ELSE FALSE END,?,?,?,?,?,?,?,?,date('now'));""", to_db)
+remove(SOURCE)
+
+con = sqlite3.connect(
+    'file:./LEMeldungen.db?mode=rw', uri=True)
+cur = con.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS le_import (col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, co13,col14,col15,col16,col17,col18,col19, col20);""")
+cur.execute("""CREATE TABLE IF NOT EXISTS le_meldungen (source TEXT NOT NULL,pzn TEXT NULL,enr TEXT NULL,meldungsart TEXT NULL,beginn TEXT NOT NULL,ende TEXT NOT NULL,datum_der_letzten_meldung TEXT NULL,art_des_grundes TEXT NOT NULL,arzneimittelbezeichnung TEXT NOT NULL,atc_code TEXT NOT NULL,wirkstoffe TEXT NOT NULL,krankenhausrelevant INTEGER NOT NULL,zulassungsinhaber TEXT NOT NULL,telefon TEXT NULL,email TEXT NULL,grund TEXT NOT NULL,anmerkung_zum_grund TEXT NULL,alternativpraeparat TEXT NULL,datum_der_erstmeldung TEXT NOT NULL,info_an_fachkreise TEXT NOT NULL,created TEXT NOT NULL);""")
+cur.execute(
+    """CREATE INDEX IF NOT EXISTS le_meldungen_created_IDX ON le_meldungen (created DESC);""")
+
+cur.executemany("""INSERT INTO le_import (col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, co13,col14,col15,col16,col17,col18,col19, col20) VALUES (?,?, ?, ?,?,?,?,?,?,?,?,CASE WHEN ? = 'ja' THEN 1 ELSE 0 END,?,?,?,?,?,?,?,?,date('now'));""", to_db)
 con.commit()
 cur.execute("""INSERT INTO le_meldungen SELECT * FROM le_import;""")
 cur.execute("""DROP TABLE IF EXISTS le_import;""")
+
 con.commit()
 con.close()
